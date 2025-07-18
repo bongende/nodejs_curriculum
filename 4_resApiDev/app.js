100db0af598aa4a70fe34ba380b8061cad701fc5
ChristoBongende <100066566+bongende@users.noreply.github.com>
2025-07-18 04:45:50 +0200

    finished to learn the basics of mongodb and mongoose.

--- 4_resApiDev/app.js
+++ 4_resApiDev/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const { DB_PASSWORD } = require("../sens_Datas");
 
 const app = express();
 
+mongoose
+  .connect(
+    `mongodb+srv://christobongende2105:${DB_PASSWORD}@cluster0.ayy1b9i.mongodb.net/`
+  )
+  .then(console.log("database connected successfully"))
+  .catch((err) => {
+    console.log(err);
+  }); // Have to remove the password from the app
+
+/*
 
 // Using the middleware 
 
@@ -59,4 +70,75 @@ app.listen(PORT, () => {
 });
 
 
+// get a single product
 
+app.get("/products/product/:id", (req, res) => {	
+	const product = products.find(product => product.id === Number(req.params.id));
+	if (!product) res.json({message : "product doesn't exist please try another id", status : 404});
+	else res.json(product);
+});
+
+
+// add a new product
+
+app.post("/add", (res, price) => {
+	products.push({id: products.length + 1, title, price});
+	res.
+
+})
+
+*/
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  isActive: Boolean,
+  tags: [String],
+  createdAt: { type: Date, default: Date.now },
+});
+
+// create User model
+
+const User = mongoose.model("User", userSchema);
+
+async function runQueryExxamples() {
+  try {
+    // Create a new Document
+
+    /*
+    
+    const newUser = await User.create({
+      name: "user1",
+      email: "user1@mail.com",
+      age: 23,
+      isActive: true,
+      tags: ["engineer", "mathematic", "sciences"],
+    });
+    
+    console.log("New user created: ", newUser);
+
+    */
+
+    const allUSers = await User.find({});
+
+    //   console.log(allUSers);
+
+    const getNonActiveUses = await User.findOne({ isActive: true });
+
+    console.log(getNonActiveUses);
+
+    const limitedUsers = await User.find()
+      .select("name email -_id")
+      .limit(3)
+      .skip(1);
+
+    console.log(limitedUsers);
+  } catch (err) {
+    console.log("Error -> ", err);
+  } finally {
+    await mongoose.connection.close();
+  }
+}
+
+runQueryExxamples();
---
+++ sens_Datas.js
@@ -0,0 +1,3 @@
+DB_PASSWORD = "christobongende2155";
+
+module.exports.DB_PASSWORD = DB_PASSWORD;
